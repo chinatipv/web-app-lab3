87c49ca1a88b2be2894d8ce525d5c34790f5d3be
chinatip <chinatip.v@ku.th>
2018-03-08 14:06:12 +0700

    Add timer

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-function loadQuestion(callback) {
-    let requestURL = "data.json"
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'text';
-    request.send();
-    request.onload = function() {
-        let questionsText = request.response;
-        let questions = JSON.parse(questionsText);
-        callback(questions);
-    }
-}
-
-// loadQuestion((d) => console.log(d));
+const timePerQuestion = 10;
+var currentQuestionTime;
 var score = 0;
 var numberQuestion;
 var totalTime;
-var timePerQuestion = 10;
 var numCurrent = 0;
+var usedTime = 0;
+var totalUsedTime = 0;
 var questions = [{ q: 'aaaa1' }, { q: 'aaaa2' }, { q: 'aaaa3' }, { q: 'aaaa4' }, { q: 'aaaa5' }, { q: 'aaaa6' }, { q: 'aaaa7' }, { q: 'aaaa8' }, { q: 'aaaa9' }, { q: 'aaaa10' }]
+var timer;
 
 function start(n) {
     numberQuestion = n;
@@ -35,6 +25,21 @@ function getQuestion() {
     for (var i = 0; i < 4; i++) {
         document.getElementById(`question-choice-${i}`).innerHTML = question.q + "  " + i;
     }
+
+    setCountdown();
+}
+
+function setCountdown() {
+    currentQuestionTime = timePerQuestion;
+    document.getElementById("timer").innerHTML = currentQuestionTime + "s";
+    timer = setInterval(function() {
+        currentQuestionTime--;
+        document.getElementById("timer").innerHTML = currentQuestionTime + "s";
+        if (currentQuestionTime === 0) {
+            clearInterval(timer);
+            next();
+        }
+    }, 1000);
 }
 
 function select() {
@@ -50,11 +55,46 @@ function hideMainMenu() {
 }
 
 function next() {
-    numCurrent++;
-    getQuestion();
+    if (numCurrent < numberQuestion - 1) {
+        clearInterval(timer);
+        document.getElementById("game-menu").style.display = "none";
+        setTimeout(function() {
+            const usedTime = timePerQuestion - currentQuestionTime;
+            totalUsedTime += usedTime;
+            console.log(totalUsedTime, usedTime, currentQuestionTime)
+            numCurrent++;
+            getQuestion();
+            document.getElementById("game-menu").style.display = "block";
+        }, 1000);
+    } else {
+        document.getElementById("game-menu").style.display = "none";
+        document.getElementById("end-menu").style.display = "block";
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("score-correct").innerHTML = score;
+        document.getElementById("score-incorrect").innerHTML = numberQuestion - score;
+        document.getElementById("total-time").innerHTML = totalUsedTime;
+        document.getElementById("average-time").innerHTML = totalUsedTime / numberQuestion;
+    }
 }
 
 function reset() {
     numberQuestion = 0;
+    numCurrent = 0;
     totalTime = 0;
+    score = 0;
+    usedTime = 0;
+    totalUsedTime = 0;
+}
+
+function loadQuestion(callback) {
+    let requestURL = "data.json"
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'text';
+    request.send();
+    request.onload = function() {
+        let questionsText = request.response;
+        let questions = JSON.parse(questionsText);
+        callback(questions);
+    }
 }
